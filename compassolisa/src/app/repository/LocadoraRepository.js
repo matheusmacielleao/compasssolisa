b68aba84a375a21d581e1aa4d68c68e05f03598d
matheusmacielleao <matheusmacielleao@gmail.com>
2021-11-05 21:19:47 -0300

    post locadora com api cpf

--- compassolisa/src/app/repository/LocadoraRepository.js
+++ compassolisa/src/app/repository/LocadoraRepository.js
@@ -3,19 +3,20 @@ const axios = require('axios').default;
 
 class LocadoraRepository {
     async create(payload) {
-        payload.endereco = payload.endereco.map(async (endereco)=>{
-            let busca = await axios.get('https://viacep.com.br/ws/96205380/json').then(function (response) {
+        for (let i = 0; i < payload.endereco.length; i++) {
+            console.log(payload.endereco[i].cep);
+            let busca = await axios.get(`https://viacep.com.br/ws/${payload.endereco[i].cep}/json`).then(function (response) {
                 return response.data;
             });
-            let { logradouro, complemento, bairro, localidade, uf,...resto} = busca; 
-            endereco.logradouro = logradouro;
-            endereco.complemento = complemento;
-            endereco.bairro = bairro;
-            endereco.localidade = localidade;
-            endereco.uf = uf;
-            return endereco;
-        });
-        return  LocadoraSchema.create(payload);
+            let { logradouro, complemento, bairro, localidade, uf, ...resto } = busca;
+            payload.endereco[i].logradouro = logradouro;
+            payload.endereco[i].complemento = complemento;
+            payload.endereco[i].bairro = bairro;
+            payload.endereco[i].localidade = localidade;
+            payload.endereco[i].uf = uf;
+        }
+        return LocadoraSchema.create(payload);
+        
     }
     async find(payload) {
         const { page = 1, limit = 100, ...query } = payload;
