3e2b7bd064943638d311ee0cc98c45bf9a2f03c7
matheusmacielleao <matheusmacielleao@gmail.com>
2021-11-09 17:35:21 -0300

    varios ajustes do eslint e patchAcessorio

--- compassolisa/src/app/repository/CarroRepository.js
+++ compassolisa/src/app/repository/CarroRepository.js
@@ -1,26 +1,42 @@
 const CarroSchema = require('../schema/CarroSchema');
-class CarroRepository {
-    async create(payload) {
-        return CarroSchema.create(payload);
-    }
-    async find(payload) {
-        const {page =1,limit=100,...query}= payload;
-        return CarroSchema.paginate(
-            {...query}, 
-            { limit: Number(limit),
-             page: Number(page),
-            skip :  (Number(page)-1)*Number(limit)});
-    }
-    async delete(payload) {
-        return CarroSchema.findByIdAndRemove({"_id":payload});
-    }
-    async findById(payload) {
-        return CarroSchema.findById(payload);
-    }
-    async update(_id,payload) {
-        return CarroSchema.findOneAndUpdate({ _id }, payload, { new: true });
-    }
 
+class CarroRepository {
+  async create(payload) {
+    return CarroSchema.create(payload);
+  }
+
+  async find(payload) {
+    const { page = 1, limit = 100, ...query } = payload;
+    return CarroSchema.paginate(
+      { ...query },
+      { limit: Number(limit), page: Number(page), skip: (Number(page) - 1) * Number(limit) }
+    );
+  }
+
+  async delete(payload) {
+    return CarroSchema.findByIdAndRemove({ _id: payload });
+  }
+
+  async findById(payload) {
+    return CarroSchema.findById(payload);
+  }
+
+  async update(_id, payload) {
+    return CarroSchema.findOneAndUpdate({ _id }, payload, { new: true });
+  }
+
+  async patchAcessorio(_id, idAcessorio, payload) {
+    const result = await CarroSchema.findOneAndUpdate(
+      { 'acessorios._id': idAcessorio },
+      {
+        $set: {
+          'acessorios.$.descricao': payload.descricao
+        }
+      },
+      { new: true, safe: true, upsert: true }
+    );
+    return result;
+  }
 }
 
 module.exports = new CarroRepository();
