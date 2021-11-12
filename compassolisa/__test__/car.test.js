f6665c173d4acbb4fdba7837655ff22a099aedca
matheusmacielleao <matheusmacielleao@gmail.com>
2021-11-12 16:49:36 -0300

    fix(Car test): eslint

--- compassolisa/__test__/car.test.js
+++ compassolisa/__test__/car.test.js
@@ -10,10 +10,10 @@ beforeAll(async () => {
   await PessoaSchema.deleteMany();
 
   const pessoaMock = {
-    nome: 'Matheus Maciel Leão',
-    cpf: '035.891.820-08',
+    nome: 'Denise Maria Maciel',
+    cpf: '359.203.353-00',
     data_nascimento: '19/06/2000',
-    email: 'matheusleao@gmail.com.com',
+    email: 'denise@gmail.com.com',
     senha: '123456',
     habilitado: 'não'
   };
@@ -30,6 +30,7 @@ beforeAll(async () => {
 
 beforeEach(async () => {
   await CarroSchema.deleteMany();
+  await PessoaSchema.deleteMany();
 });
 
 describe('criar carro', () => {
@@ -136,5 +137,43 @@ describe('listar carro por Id', () => {
     expect(body.quantidadePassageiros).toBe(carroMock.quantidadePassageiros);
     const { status } = response;
     expect(status).toBe(200);
+  });
+});
+
+describe('deletar carro', () => {
+  it('retornar corpo em branco e status 204', async () => {
+    const carroMock = {
+      modelo: 'Fusca',
+      cor: 'Azul',
+      ano: 2010,
+      acessorios: [
+        {
+          descricao: 'Piloto Automatico'
+        },
+        {
+          descricao: 'Trava Eletrica'
+        }
+      ],
+      quantidadePassageiros: 10
+    };
+
+    const { text } = await request(app)
+      .post('/api/v1/car/')
+      .set('Authorization', `Bearer ${BearerToken}`)
+      .send(carroMock);
+
+    const { _id } = JSON.parse(text);
+
+    const response = await request(app)
+      .delete(`/api/v1/car/${_id.toString()}`)
+      .set('Authorization', `Bearer ${BearerToken}`);
+    const { body } = response;
+    expect(body.modelo).toBeUndefined();
+    expect(body.cor).toBeUndefined();
+    expect(body.ano).toBeUndefined();
+    expect(body.acessorios).toBeUndefined();
+    expect(body.quantidadePassageiros).toBeUndefined();
+    const { status } = response;
+    expect(status).toBe(204);
   });
 });
