1314fc52fc5c43bcd0de4ad62fd469c629cea27a
matheusmacielleao <matheusmacielleao@gmail.com>
2021-11-18 17:36:24 -0300

    refactor(create  Locadora): Promisse all para os await usando o axios

--- compassolisa/src/app/repository/LocadoraRepository.js
+++ compassolisa/src/app/repository/LocadoraRepository.js
@@ -3,11 +3,17 @@ const LocadoraSchema = require('../schema/LocadoraSchema');
 
 class LocadoraRepository {
   async create(payload) {
+    const enderecos = [];
+
+    payload.endereco.forEach((endereco) => {
+      enderecos.push(axios.get(`https://viacep.com.br/ws/${endereco.cep}/json`).then((response) => response.data));
+    });
+
+    const enderecosFinais = await Promise.all(enderecos);
+
     for (let i = 0; i < payload.endereco.length; i++) {
       // eslint-disable-next-line no-await-in-loop
-      const busca = await axios
-        .get(`https://viacep.com.br/ws/${payload.endereco[i].cep}/json`)
-        .then((response) => response.data);
+      const busca = enderecosFinais[i];
       const { logradouro, complemento, bairro, localidade, uf } = busca;
       payload.endereco[i].logradouro = logradouro;
       payload.endereco[i].complemento = complemento;
