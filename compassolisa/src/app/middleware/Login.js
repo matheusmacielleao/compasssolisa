bff38973df5123e035dc242632bda77dc119ae97
matheusmacielleao <matheusmacielleao@gmail.com>
2021-11-19 10:25:48 -0300

    fix(secret): usando secret do .env

--- compassolisa/src/app/middleware/Login.js
+++ compassolisa/src/app/middleware/Login.js
@@ -1,10 +1,9 @@
 const jwt = require('jsonwebtoken');
-const authConfig = require('../../config/auth.json');
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decode = jwt.verify(token, authConfig.secret);
+    const decode = jwt.verify(token, process.env.SECRET);
     req.usuario = decode;
     return next();
   } catch (error) {
