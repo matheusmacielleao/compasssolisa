bd1211751984de0195cea788298b4e6bf993dd36
matheusmacielleao <matheusmacielleao@gmail.com>
2021-11-19 18:22:07 -0300

    fix(idValidation): Agora feita usando Joi

--- compassolisa/src/app/validation/idValidation.js
+++ compassolisa/src/app/validation/idValidation.js
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose');
+const Joi = require('joi');
+const ErroSerialize = require('../serialize/ErroSerialize');
+const { regexId } = require('../utils/regex');
 
 module.exports = async (req, res, next) => {
   try {
-    const error = await mongoose.Types.ObjectId.isValid(req.params.id);
-    if (!error) throw error;
+    const schema = Joi.object({
+      _id: Joi.string().regex(regexId()).required()
+    });
+
+    const { error } = await schema.validate({ _id: req.params }, { abortEarly: false });
+    if (error) throw error;
     return next();
   } catch (error) {
-    return res.status(400).json('Id diferente do Padrão');
+    return res.status(400).json(ErroSerialize(error));
   }
 };
