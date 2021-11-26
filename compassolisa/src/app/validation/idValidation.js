10c9bfa50fca0b2d6c305ac067a44b915683fd2d
matheusmacielleao <matheusmacielleao@gmail.com>
2021-11-26 09:07:13 -0300

    fix(idValidation): não estava funcionando corretamente

--- compassolisa/src/app/validation/idValidation.js
+++ compassolisa/src/app/validation/idValidation.js
@@ -5,10 +5,10 @@ const { regexId } = require('../utils/regex');
 module.exports = async (req, res, next) => {
   try {
     const schema = Joi.object({
-      _id: Joi.string().regex(regexId()).required()
+      _id: Joi.array().items(Joi.string().regex(regexId()).required())
     });
 
-    const { error } = await schema.validate({ _id: req.params }, { abortEarly: false });
+    const { error } = await schema.validate({ _id: req.params._id }, { abortEarly: false });
     if (error) throw error;
     return next();
   } catch (error) {
